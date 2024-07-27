0443542fb56d7102d985847ebabd53634a442a84
Hackathon2024PILARES <hackathon2024pilares@gmail.com>
2024-07-26 19:54:52 -0600

    Logo

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/*import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getDatabase, ref, set, get, child, update } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+
+const firebaseConfig = {
+            apiKey: "AIzaSyD-DM0yFWbFpeIGF3eTSpDo8gOEvitRXkM",
+            authDomain: "pruebahackathon-eea94.firebaseapp.com",
+            projectId: "pruebahackathon-eea94",
+            storageBucket: "pruebahackathon-eea94.appspot.com",
+            messagingSenderId: "743812823032",
+            appId: "1:743812823032:web:0d105ab1c5f02d52e6480d"
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const db = getDatabase(app);*/
+
 function iniciarCuestionario(){
     let botonRespuestaUsuario=document.getElementById('boton-respuesta')
     botonRespuestaUsuario.addEventListener('click', seleccionarRespuesta)
@@ -5,6 +21,7 @@ function iniciarCuestionario(){
 function seleccionarRespuesta(){
     let pregunta1, pregunta2, pregunta3, pregunta4, pregunta5
     let puntaje = 20
+    let usuario = document.getElementById("usuario").value
     let inputrespuestauno=document.getElementById('1')
     let inputrespuestados=document.getElementById('2')
     let inputrespuestatres=document.getElementById('3')
@@ -25,92 +42,95 @@ function seleccionarRespuesta(){
     let inputrespuestacincodos=document.getElementById('52')
     let inputrespuestacincotres=document.getElementById('53')
     let inputrespuestacincocuatro=document.getElementById('54')
-    if (inputrespuestauno.checked) {
-        document.getElementById("correcta1").style.background = "#8aff7ace"
-        pregunta1 = 1
-    } else if (inputrespuestados.checked) {
-        document.getElementById("correcta1").style.background = "#8aff7ace"
-        pregunta1 = 1
-    } else if (inputrespuestatres.checked) {
-        puntaje = puntaje + 16
-        pregunta1 = 1
-    } else if (inputrespuestacuatro.checked) {
-        document.getElementById("correcta1").style.background = "#8aff7ace"
-        pregunta1 = 1
+    if (usuario === ""){
+        alert("Ingresa un nombre de usuario")
+        return;
     } else {
-        alert("Responde la pregunta 1")
+        if (inputrespuestauno.checked) {
+            document.getElementById("correcta1").style.background = "#8aff7ace"
+            pregunta1 = 1
+        } else if (inputrespuestados.checked) {
+            document.getElementById("correcta1").style.background = "#8aff7ace"
+            pregunta1 = 1
+        } else if (inputrespuestatres.checked) {
+            puntaje = puntaje + 16
+            pregunta1 = 1
+        } else if (inputrespuestacuatro.checked) {
+            document.getElementById("correcta1").style.background = "#8aff7ace"
+            pregunta1 = 1
+        } else {
+            alert("Responde la pregunta 1")
+        }
+        if (inputrespuestadosuno.checked) {
+            document.getElementById("correcta2").style.background = "#8aff7ace"
+            pregunta2 = 1
+        } else if (inputrespuestadosdos.checked) {
+            puntaje = puntaje + 16
+            pregunta2 = 1
+        } else if (inputrespuestadostres.checked) {
+            document.getElementById("correcta2").style.background = "#8aff7ace"
+            pregunta2 = 1
+        } else if (inputrespuestadoscuatro.checked) {
+            document.getElementById("correcta2").style.background = "#8aff7ace"
+            pregunta2 = 1
+        } else {
+            alert("Responde la pregunta 2")
+        }
+        if (inputrespuestatresuno.checked) {
+            puntaje = puntaje + 16
+            pregunta3 = 1
+        } else if (inputrespuestatresdos.checked) {
+            document.getElementById("correcta3").style.background = "#8aff7ace"
+            pregunta3 = 1
+        } else if (inputrespuestatrestres.checked) {
+            document.getElementById("correcta3").style.background = "#8aff7ace"
+            pregunta3 = 1
+        } else if (inputrespuestatrescuatro.checked) {
+            document.getElementById("correcta3").style.background = "#8aff7ace"
+            pregunta3 = 1
+        } else {
+            alert("Responde la pregunta 3")
+        }
+        if (inputrespuestacuatrouno.checked) {
+            document.getElementById("correcta4").style.background = "#8aff7ace"
+            pregunta4 = 1
+        } else if (inputrespuestacuatrodos.checked) {
+            document.getElementById("correcta4").style.background = "#8aff7ace"
+            pregunta4 = 1
+        } else if (inputrespuestacuatrotres.checked) {
+            puntaje = puntaje + 16
+            pregunta4 = 1
+        } else if (inputrespuestacuatrocuatro.checked) {
+            document.getElementById("correcta4").style.background = "#8aff7ace"
+            pregunta4 = 1
+        } else {
+            alert("Responde la pregunta 4")
+        }
+        if (inputrespuestacincouno.checked) {
+            document.getElementById("correcta5").style.background = "#8aff7ace"
+            pregunta5 = 1
+        } else if (inputrespuestacincodos.checked) {
+            document.getElementById("correcta5").style.background = "#8aff7ace"
+            pregunta5 = 1
+        } else if (inputrespuestacincotres.checked) {
+            document.getElementById("correcta5").style.background = "#8aff7ace"
+            pregunta5 = 1
+        } else if (inputrespuestacincocuatro.checked) {
+            puntaje = puntaje + 16
+            pregunta5 = 1
+        } else {
+            alert("Responde la pregunta 5")
+        }
+        if (pregunta1 == 1 && pregunta2 == 1 && pregunta3 == 1 && pregunta4 == 1 && pregunta5 == 1 ){
+            alert("¡" + usuario + " has obtenido un puntaje de " + puntaje + "!")
+        } else {
+            document.getElementById("correcta1").style.background = "#f0e8da"
+            document.getElementById("correcta2").style.background = "#f0e8da"
+            document.getElementById("correcta3").style.background = "#f0e8da"
+            document.getElementById("correcta4").style.background = "#f0e8da"
+            document.getElementById("correcta5").style.background = "#f0e8da"
+        }
     }
-    if (inputrespuestadosuno.checked) {
-        document.getElementById("correcta2").style.background = "#8aff7ace"
-        pregunta2 = 1
-    } else if (inputrespuestadosdos.checked) {
-        puntaje = puntaje + 16
-        pregunta2 = 1
-    } else if (inputrespuestadostres.checked) {
-        document.getElementById("correcta2").style.background = "#8aff7ace"
-        pregunta2 = 1
-    } else if (inputrespuestadoscuatro.checked) {
-        document.getElementById("correcta2").style.background = "#8aff7ace"
-        pregunta2 = 1
-    } else {
-        alert("Responde la pregunta 2")
-    }
-    if (inputrespuestatresuno.checked) {
-        puntaje = puntaje + 16
-        pregunta3 = 1
-    } else if (inputrespuestatresdos.checked) {
-        document.getElementById("correcta3").style.background = "#8aff7ace"
-        pregunta3 = 1
-    } else if (inputrespuestatrestres.checked) {
-        document.getElementById("correcta3").style.background = "#8aff7ace"
-        pregunta3 = 1
-    } else if (inputrespuestatrescuatro.checked) {
-        document.getElementById("correcta3").style.background = "#8aff7ace"
-        pregunta3 = 1
-    } else {
-        alert("Responde la pregunta 3")
-    }
-    if (inputrespuestacuatrouno.checked) {
-        document.getElementById("correcta4").style.background = "#8aff7ace"
-        pregunta4 = 1
-    } else if (inputrespuestacuatrodos.checked) {
-        document.getElementById("correcta4").style.background = "#8aff7ace"
-        pregunta4 = 1
-    } else if (inputrespuestacuatrotres.checked) {
-        puntaje = puntaje + 16
-        pregunta4 = 1
-    } else if (inputrespuestacuatrocuatro.checked) {
-        document.getElementById("correcta4").style.background = "#8aff7ace"
-        pregunta4 = 1
-    } else {
-        alert("Responde la pregunta 4")
-    }
-    if (inputrespuestacincouno.checked) {
-        document.getElementById("correcta5").style.background = "#8aff7ace"
-        pregunta5 = 1
-    } else if (inputrespuestacincodos.checked) {
-        document.getElementById("correcta5").style.background = "#8aff7ace"
-        pregunta5 = 1
-    } else if (inputrespuestacincotres.checked) {
-        document.getElementById("correcta5").style.background = "#8aff7ace"
-        pregunta5 = 1
-    } else if (inputrespuestacincocuatro.checked) {
-        puntaje = puntaje + 16
-        pregunta5 = 1
-    } else {
-        alert("Responde la pregunta 5")
-    }
-    if (pregunta1 == 1 && pregunta2 == 1 && pregunta3 == 1 && pregunta4 == 1 && pregunta5 == 1 ){
-        alert("Puntaje: " + puntaje)
-    } else {
-        document.getElementById("correcta1").style.background = "white"
-        document.getElementById("correcta2").style.background = "white"
-        document.getElementById("correcta3").style.background = "white"
-        document.getElementById("correcta4").style.background = "white"
-        document.getElementById("correcta5").style.background = "white"
-    }
-
-
 }
 
 window.addEventListener('load', iniciarCuestionario)
